3f3236d3a3fa331f51c34854635e61fc572985d8
ksilva <kevinsq16@gmail.com>
2022-04-18 17:16:16 -0400

    setup project in vscode

---
+++ src/main/java/com/personal/BasicStack.java
@@ -0,0 +1,21 @@
+package com.personal;
+
+public class BasicStack<T> {
+
+    private T[] items;
+    private int stackIndex;
+
+    public BasicStack() {
+        items = (T[]) new Object[1000];
+        stackIndex = 0;
+    }
+
+    public void push(T newItem) {
+
+    }
+
+    public T pop() {
+
+    }
+    
+}
