81daa32450a39a7e343afe23d04e1903a21cd53f
ksilva <kevinsq16@gmail.com>
2022-04-24 17:30:20 -0400

    Stack unit tests.

--- src/main/java/com/personal/BasicStack.java
+++ src/main/java/com/personal/BasicStack.java
@@ -11,11 +11,45 @@ public class BasicStack<T> {
     }
 
     public void push(T newItem) {
-
+        items[++stackIndex] = newItem;
     }
 
     public T pop() {
+        if(stackIndex == 0) {
+            System.out.println("Cannot pop from empty Stack");
+            return null;
+        }
 
+        return items[stackIndex--];
+    }
+
+    public boolean contains(T searchedItem) {
+        for(int i = stackIndex; i> 0 ; i--) {
+            if(items[i].equals(searchedItem)) {
+                return true;
+            }
+        } 
+
+        return false;
+    }
+
+    public T access(T searchedItem) {
+        for (int i = 0; i < stackIndex ; i++) {
+            T currentItem = items[i];
+            if(currentItem.equals(searchedItem)) {
+                // found item
+                return currentItem;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " items='" + items + "'" +
+            ", stackIndex='" + stackIndex + "'" +
+            "}";
     }
     
 }
---
+++ src/test/java/com/personal/BasicStackTest.java
@@ -0,0 +1,34 @@
+package com.personal;
+
+
+import org.junit.jupiter.api.Test;
+
+public class BasicStackTest {
+
+
+    @Test
+    void testAccess() {
+
+        BasicStack stack = new BasicStack<Integer>();
+        stack.push(5);
+
+        assertThat()
+    
+
+    }
+
+    @Test
+    void testContains() {
+
+    }
+
+    @Test
+    void testPop() {
+
+    }
+
+    @Test
+    void testPush() {
+
+    }
+}
